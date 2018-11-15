c64c702ab41081e6a750026107c7901c373aa525
oli <olivier.arbellay@students.hevs.ch>
2018-11-15 18:37:43 +0100

    Bug correction on login logic

--- app/src/main/java/ch/hes/it/higiv/MainActivity.java
+++ app/src/main/java/ch/hes/it/higiv/MainActivity.java
@@ -1,13 +1,67 @@
 package ch.hes.it.higiv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+    private Button btnSignout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // User is logged in
+        }
+
+        btnSignout = (Button) findViewById(R.id.sign_out);
+
+
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+
+    }
+    //sign out method
+    public void signOut() {
+        auth.signOut();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+
+        }
     }
 }
