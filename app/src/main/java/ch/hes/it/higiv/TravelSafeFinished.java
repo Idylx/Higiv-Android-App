036589caac64153efa1c242e456a5a829a91b261
Jaufnet6 <jaufray.sornette@gmail.com>
2018-11-27 15:47:43 +0100

    First fragment changed and works between the 2 fragments

---
+++ app/src/main/java/ch/hes/it/higiv/TravelSafeFinished.java
@@ -0,0 +1,24 @@
+package ch.hes.it.higiv;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TravelSafeFinished extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_travel_safe_finished, container, false);
+
+
+
+        return  view;
+    }
+
+}
