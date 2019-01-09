7aff2394f50c32219eeb998109e021b3a4412af1
Tink711 <quentin.remion@students.hevs.ch>
2019-01-09 08:55:51 +0100

    Add splash screen

---
+++ app/src/main/java/ch/hes/it/higiv/SplashScreen.java
@@ -0,0 +1,49 @@
+package ch.hes.it.higiv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import ch.hes.it.higiv.Account.LoginActivity;
+
+public class SplashScreen extends Activity {
+
+    Thread splashTread;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        imageView = (ImageView)findViewById(R.id.SplashLogo);
+
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 1800) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this,
+                            LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+    }
+
+}
